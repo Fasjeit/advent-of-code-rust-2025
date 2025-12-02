727569fba035bcfe4036cc5d8e4ab93eb8fbd114
Fasjeit <15075638+Fasjeit@users.noreply.github.com>
2025-12-02 10:03:25 +0300

    🎅 day 2 solved! 🎄 (no so happy about performance)

---
+++ src/bin/02.rs
@@ -0,0 +1,218 @@
+advent_of_code::solution!(2);
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let ranges_str: Vec<&str> = input.trim_end().split(',').collect();
+    let ranges = ranges_str.iter().map(|s| {
+        let mut split = s.split('-');
+        Range {
+            start_index: split.next().unwrap().parse().unwrap(),
+            end_index: split.next().unwrap().parse().unwrap(),
+        }
+    });
+
+    Some(ranges.fold(0, |acc, x| acc + count_invalid(x, test_invalid)))
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let ranges_str: Vec<&str> = input.trim_end().split(',').collect();
+    let ranges = ranges_str.iter().map(|s| {
+        let mut split = s.split('-');
+        Range {
+            start_index: split.next().unwrap().parse().unwrap(),
+            end_index: split.next().unwrap().parse().unwrap(),
+        }
+    });
+
+    Some(ranges.fold(0, |acc, x| acc + count_invalid(x, test_invalid_part2)))
+}
+
+fn count_invalid(range: Range, test_invalid: fn(u64) -> bool) -> u64 {
+    let mut res = 0;
+    for i in range.start_index..range.end_index + 1 {
+        if test_invalid(i) {
+            res += i;
+        }
+    }
+
+    res
+}
+
+fn test_invalid(input: u64) -> bool {
+    let digits = get_digits(input);
+    if !digits.len().is_multiple_of(2) {
+        return false;
+    }
+
+    let median = digits.len() / 2;
+    for i in 0..median {
+        if digits[i] != digits[i + median] {
+            return false;
+        }
+    }
+    true
+}
+
+fn test_invalid_part2(input: u64) -> bool {
+    let digits = get_digits(input);
+
+    // test every `part` digits
+
+    // ex part_len = 2
+    // 1 2 3 4 5 6
+    // test 1 == 3 == 6 (part index 1, 2)
+    // and
+    // test 2 == 4 == 6 (part index 1, 2)
+
+    'outer: for pattern_len in 1..digits.len() {
+        //dbg!(&pattern_len);
+        if !digits.len().is_multiple_of(pattern_len) {
+            continue;
+        }
+
+        for pattern_index in 0..pattern_len {
+            //dbg!(&i);
+            // check every `pattern_len` digit
+            for part_index in 1..digits.len() / pattern_len {
+                if digits[pattern_index] != digits[pattern_index + part_index * pattern_len] {
+                    continue 'outer;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    false
+}
+
+fn get_digits(mut n: u64) -> Vec<u64> {
+    if n == 0 {
+        return vec![0];
+    }
+    let mut digits = Vec::new();
+    while n > 0 {
+        digits.push(n % 10);
+        n /= 10;
+    }
+    digits.into_iter().rev().collect()
+}
+
+struct Range {
+    start_index: u64,
+    end_index: u64,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_invalid1() {
+        let result = test_invalid(1212);
+        assert!(result);
+    }
+
+    #[test]
+    fn test_invalid2() {
+        let result = test_invalid(12012);
+        assert!(!result);
+    }
+
+    #[test]
+    fn test_invalid3() {
+        let result = test_invalid(124124);
+        assert!(result);
+    }
+
+    #[test]
+    fn test_invalid4() {
+        let result = test_invalid(1240124);
+        assert!(!result);
+    }
+
+    #[test]
+    fn test_invalid_part2_1() {
+        let result = test_invalid_part2(1212);
+        assert!(result);
+    }
+
+    #[test]
+    fn test_invalid_part2_2() {
+        let result = test_invalid_part2(12012);
+        assert!(!result);
+    }
+
+    #[test]
+    fn test_invalid_part2_3() {
+        let result = test_invalid_part2(124124);
+        assert!(result);
+    }
+
+    #[test]
+    fn test_invalid_part2_4() {
+        let result = test_invalid_part2(1240124);
+        assert!(!result);
+    }
+
+    #[test]
+    fn test_invalid_part2_5() {
+        let result = test_invalid_part2(111);
+        assert!(result);
+    }
+
+    #[test]
+    fn test_invalid_part2_6() {
+        let result = test_invalid_part2(123123123);
+        assert!(result);
+    }
+
+    #[test]
+    fn test_invalid_part2_7() {
+        let result = test_invalid_part2(1212121212);
+        assert!(result);
+    }
+
+    #[test]
+    fn test_invalid_part2_8() {
+        let result = test_invalid_part2(100);
+        assert!(!result);
+    }
+
+    #[test]
+    fn test_invalid_part2_9() {
+        let result = test_invalid_part2(1011);
+        assert!(!result);
+    }
+
+    #[test]
+    fn test_invalid_part2_10() {
+        let result = test_invalid_part2(9999);
+        assert!(result);
+    }
+
+    #[test]
+    fn test_invalid_part2_11() {
+        let result = test_invalid_part2(1188511880);
+        assert!(!result);
+    }
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file_input(
+            "examples",
+            DAY,
+            "example_1.txt",
+        ));
+        assert_eq!(result, Some(1227775554));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file_input(
+            "examples",
+            DAY,
+            "example_1.txt",
+        ));
+        assert_eq!(result, Some(4174379265));
+    }
+}
