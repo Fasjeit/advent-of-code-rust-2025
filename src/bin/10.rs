dde4e68c78847028c94daf761aa0c3574846fca2
Fasjeit <15075638+Fasjeit@users.noreply.github.com>
2025-12-11 05:23:03 +0300

    🎅 day 10 solved! 🎄 (with a lot of extra help...)

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,8 +1,6 @@
 use std::{
     collections::{HashMap, VecDeque},
-    env::current_exe,
     fmt::Display,
-    panic::RefUnwindSafe,
 };
 
 advent_of_code::solution!(10);
@@ -11,30 +9,13 @@ pub fn part_one(input: &str) -> Option<u64> {
     let lines = input.trim_end().lines();
     let mut result = 0;
     for (i, l) in lines.enumerate() {
-        let mut splitted_1 = l.split("]");
-        let state = splitted_1.next().unwrap();
-        let mut machine = Machine::from(state);
-        // println!("{}", machine);
-        // println!("{}", machine.get_score());
-        // machine.push_button(&Button {
-        //     states_to_change: vec![0, 1, 2],
-        // });
-        // println!("{}", machine);
-        // println!("{}", machine.get_score());
-
-        let mut splitted_2 = splitted_1.next().unwrap().trim_end().split("{");
-        let buttons: Vec<Button> = splitted_2
-            .next()
-            .unwrap()
-            .split_whitespace()
-            .map(Button::from)
-            .collect();
+        let machine = Machine::from(l);
 
         let mut visited = HashMap::new();
         let iteration_result = bfs(
-            &machine.current_state,
+            &vec![false; machine.required_state.len()],
             &machine.required_state,
-            &buttons,
+            &machine.buttons,
             &mut visited,
         )
         .unwrap();
@@ -49,47 +30,31 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
+    // I've just gave up here...
+    // the solution below is from
+    // https://www.reddit.com/r/adventofcode/comments/1pity70/comment/ntd2evv/
+    // https://gist.github.com/icub3d/16eea2a8b4a94d193a148fef908779a9
+    // https://www.youtube.com/watch?v=xibCHVRF6oI
+
     let lines = input.trim_end().lines();
     let mut result = 0;
-    for (i, l) in lines.enumerate() {
-        let mut splitted_1 = l.split("]");
-        let state = splitted_1.next().unwrap();
-        let mut machine = Machine::from(state);
-        // println!("{}", machine);
-        // println!("{}", machine.get_score());
-        // machine.push_button(&Button {
-        //     states_to_change: vec![0, 1, 2],
-        // });
-        // println!("{}", machine);
-        // println!("{}", machine.get_score());
+    for l in lines {
+        let machine = Machine::from(l);
 
-        let mut splitted_2 = splitted_1.next().unwrap().trim_end().split("{");
-        let buttons: Vec<Button> = splitted_2
-            .next()
-            .unwrap()
-            .split_whitespace()
-            .map(Button::from)
-            .collect();
-        let joltage = Joltage::from(splitted_2.next().unwrap());
+        let matrix = Matrix::from_machine(&machine);
 
-        let initial_state = vec![0; joltage.target_state.len()];
+        // Now we can DFS over a much smaller solution space.
+        let max = machine.joltage.target_state.iter().max().unwrap() + 1;
+        let mut min = usize::MAX;
+        let mut values = vec![0; matrix.independents.len()];
 
-        let mut visited = HashMap::new();
-        let iteration_result = bfs_2(
-            &initial_state,
-            &joltage.target_state,
-            &buttons,
-            &mut visited,
-        )
-        .unwrap();
+        dfs(&matrix, 0, &mut values, &mut min, max as usize);
+        result += min;
 
-        result += iteration_result;
-
-        //dbg!(visited.get(&machine.required_state));.
-        println!("{} / 193", i + 1);
+        //println!("{} / 193", i + 1);
     }
 
-    Some(result)
+    Some(result as u64)
 }
 
 fn bfs(
@@ -160,6 +125,8 @@ fn bfs(
     None
 }
 
+// Working for part 2, but too slow :(
+#[allow(dead_code)]
 fn bfs_2(
     initial_state: &Vec<u64>,
     target_state: &Vec<u64>,
@@ -238,13 +205,157 @@ fn bfs_2(
     None
 }
 
-fn get_score(current_state: &Vec<bool>, target_state: &Vec<bool>) -> u64 {
-    target_state.iter().enumerate().fold(
-        0,
-        |acc, (i, x)| {
-            if current_state[i] == *x { acc + 1 } else { acc }
-        },
-    )
+struct Matrix {
+    data: Vec<Vec<f64>>,
+    rows: usize,
+    cols: usize,
+    dependents: Vec<usize>,
+    independents: Vec<usize>,
+}
+
+impl Matrix {
+    const EPSILON: f64 = 1e-9;
+
+    // Make a matrix, do a Gaussian elimination and setup the fixed and free variables.
+    fn from_machine(machine: &Machine) -> Self {
+        let rows = machine.joltage.target_state.len();
+        let cols = machine.buttons.len();
+        let mut data = vec![vec![0.0; cols + 1]; rows];
+
+        // Add all of our buttons.
+        for (c, button) in machine.buttons.iter().enumerate() {
+            for r in &button.states_to_change {
+                if *r < rows {
+                    data[*r][c] = 1.0;
+                }
+            }
+        }
+
+        // Add our joltages to the last column
+        for (r, &val) in machine.joltage.target_state.iter().enumerate() {
+            data[r][cols] = val as f64;
+        }
+
+        let mut matrix = Self {
+            data,
+            rows,
+            cols,
+            dependents: Vec::new(),
+            independents: Vec::new(),
+        };
+
+        matrix.gaussian_elimination();
+        matrix
+    }
+
+    // https://en.wikipedia.org/wiki/Gaussian_elimination
+    fn gaussian_elimination(&mut self) {
+        let mut pivot = 0;
+
+        let mut col = 0;
+        while pivot < self.rows && col < self.cols {
+            // Find the best pivot row for this column.
+            let (best_row, best_value) = self
+                .data
+                .iter()
+                .enumerate()
+                .skip(pivot)
+                .map(|(r, row)| (r, row[col].abs()))
+                .max_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap())
+                .unwrap();
+
+            // If the best value is zero, this is a free variable.
+            if best_value < Self::EPSILON {
+                self.independents.push(col);
+                col += 1;
+                continue;
+            }
+
+            // Swap rows and mark this column as dependent.
+            self.data.swap(pivot, best_row);
+            self.dependents.push(col);
+
+            // Normalize pivot row.
+            let pivot_value = self.data[pivot][col];
+            for val in &mut self.data[pivot][col..=self.cols] {
+                *val /= pivot_value;
+            }
+
+            // Eliminate this column in all other rows.
+            for r in 0..self.rows {
+                if r != pivot {
+                    let factor = self.data[r][col];
+                    if factor.abs() > Self::EPSILON {
+                        let pivot_row = self.data[pivot][col..=self.cols].to_vec();
+                        self.data[r][col..=self.cols]
+                            .iter_mut()
+                            .zip(&pivot_row)
+                            .for_each(|(val, &pivot_val)| {
+                                *val -= factor * pivot_val;
+                            });
+                    }
+                }
+            }
+
+            pivot += 1;
+            col += 1;
+        }
+
+        // Any remaining columns are free variables
+        self.independents.extend(col..self.cols);
+    }
+
+    // Check if the given values for our independent variables are valid. If so, return the total button presses.
+    fn valid(&self, values: &[usize]) -> Option<usize> {
+        // We start with how many times we've pressed the free variables.
+        let mut total = values.iter().sum::<usize>();
+
+        // Calculate dependent variable values based on independent variables.
+        for row in 0..self.dependents.len() {
+            // Calculate this dependent by subtracting the sum of the free variable pushes from the solution.
+            let val = self
+                .independents
+                .iter()
+                .enumerate()
+                .fold(self.data[row][self.cols], |acc, (i, &col)| {
+                    acc - self.data[row][col] * (values[i] as f64)
+                });
+
+            // We need non-negative, whole numbers for a valid solution.
+            if val < -Self::EPSILON {
+                return None;
+            }
+            let rounded = val.round();
+            if (val - rounded).abs() > Self::EPSILON {
+                return None;
+            }
+
+            total += rounded as usize;
+        }
+
+        Some(total)
+    }
+}
+
+fn dfs(matrix: &Matrix, idx: usize, values: &mut [usize], min: &mut usize, max: usize) {
+    // When we've assigned all independent variables, check if it's a valid solution.
+    if idx == matrix.independents.len() {
+        if let Some(total) = matrix.valid(values) {
+            *min = (*min).min(total);
+        }
+        return;
+    }
+
+    // Try different values for the current independent variable.
+    let total: usize = values[..idx].iter().sum();
+    for val in 0..max {
+        // Optimization: If we ever go above our min, we can't possibly do better.
+        if total + val >= *min {
+            break;
+        }
+        values[idx] = val;
+        dfs(matrix, idx + 1, values, min, max);
+    }
 }
 
 #[derive(Debug)]
@@ -285,37 +396,25 @@ impl From<&str> for Joltage {
 
 struct Machine {
     required_state: Vec<bool>,
-    current_state: Vec<bool>,
-}
-
-impl Machine {
-    fn get_score(&self) -> u64 {
-        self.required_state
-            .iter()
-            .enumerate()
-            .fold(0, |acc, (i, x)| {
-                if self.current_state[i] == *x {
-                    acc + 1
-                } else {
-                    acc
-                }
-            })
-    }
-
-    fn push_button(&self, button: &Button) -> Vec<bool> {
-        let mut result = vec![];
-        button
-            .states_to_change
-            .iter()
-            .for_each(|s| result[*s] = !self.current_state[*s]);
-        result
-    }
+    buttons: Vec<Button>,
+    joltage: Joltage,
 }
 
 impl From<&str> for Machine {
     fn from(value: &str) -> Self {
+        let mut splitted_1 = value.split("]");
+        let state = splitted_1.next().unwrap();
+        let mut splitted_2 = splitted_1.next().unwrap().trim_end().split("{");
+        let buttons: Vec<Button> = splitted_2
+            .next()
+            .unwrap()
+            .split_whitespace()
+            .map(Button::from)
+            .collect();
+        let joltage = Joltage::from(splitted_2.next().unwrap());
+
         let mut required_state: Vec<bool> = vec![];
-        for char in value.chars() {
+        for char in state.chars() {
             match char {
                 '[' => continue,
                 ']' => break,
@@ -325,33 +424,22 @@ impl From<&str> for Machine {
             }
         }
 
-        let state_len = required_state.len();
         Machine {
             required_state,
-            current_state: vec![false; state_len],
+            buttons,
+            joltage,
         }
     }
 }
 
 impl Display for Machine {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let mut current_state_string = "".to_string();
-        self.current_state.iter().for_each(|c| match c {
-            false => current_state_string += ".",
-            true => current_state_string += "#",
-        });
-
         let mut target_state_string = "".to_string();
         self.required_state.iter().for_each(|c| match c {
             false => target_state_string += ".",
             true => target_state_string += "#",
         });
-        // state_string = state_string.chars().rev().collect();
-        write!(
-            f,
-            "[{}] - req [{}]",
-            current_state_string, target_state_string
-        )
+        write!(f, "[{}]", target_state_string)
     }
 }
 
